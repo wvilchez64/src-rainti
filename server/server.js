92557d6b559ab6fca552b6ae2019eb275329cf63
allanrainti <allan.finco@rainti.com.br>
2019-12-30 15:29:08 +0000

    commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const port = 3000
+const api = require('./routes/api')
+const app = express()
+
+app.use(cors())
+
+app.use(bodyParser.json())
+
+app.use('/api',api)
+
+app.get('/', function(req,res){
+    res.send('Hello Server')
+})
+
+app.listen(port, function(){
+    console.log('Server is running on localhost:'+port)
+})
