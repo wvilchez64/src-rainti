9da803930c8479eb29a6f471ae97381779fcc79b
allanrainti <allan.finco@rainti.com.br>
2020-01-02 18:35:17 +0000

    commit

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,8 +5,8 @@ const email = require('../model/mailing')
 const router = express.Router()
 
 // GETS
-router.get('/entities', queries.getDetrans)
-router.get('/entities-types', queries.getEntitiesTypes)
+router.get('/entities', verifyToken, queries.getDetrans)
+router.get('/entities-types', verifyToken, queries.getEntitiesTypes)
 router.get('/', (req, res)=>{res.send('From API route')})
 
 // POSTs
@@ -15,5 +15,21 @@ router.post('/reset-password', queries.resetPassword)
 router.post('/recover-password', queries.recoverPassword)
 router.post('/register', queries.createUser)
 router.post('/login', queries.loginUser)
+
+function verifyToken(req, res, next){
+    if(!req.headers.authorization){
+        return res.status(401).send('Unauthorized request')
+    }
+    let token = req.headers.authorization.split(' ')[1]
+    if(token === 'null'){
+        return res.status(401).send('Unauthorized request')
+    }
+    let payload = jwt.verify(token, 'secretKey')
+    if(!payload){
+        return res.status(401).send('Unauthorized request')
+    }
+    req.userId = payload.subject
+    next()
+}
 
 module.exports = router
