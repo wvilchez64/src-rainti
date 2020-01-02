296592a12c6b9039c55ea9cf1cf7be880be68e99
allanrainti <allan.finco@rainti.com.br>
2020-01-02 16:01:16 +0000

    commit

--- server/model/test.js
+++
@@ -1,4 +0,0 @@
-const email = require('./mailing')
-const emailConfig = require('./config-email.json')
-
-email.sendEmail(emailConfig.host, emailConfig.port, emailConfig.secure, emailConfig.userName, emailConfig.emailFrom, emailConfig.password, "allan.finco@rainti.com.br","email no reply","testando").catch(console.error);
