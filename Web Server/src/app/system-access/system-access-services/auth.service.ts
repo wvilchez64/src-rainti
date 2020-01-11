08a03b1530d796f02e7e1b292bb4c0de70e631cb
allanrainti <allan.finco@rainti.com.br>
2020-01-11 14:36:49 +0000

    commit

--- Web Server/src/app/system-access/system-access-services/auth.service.ts
+++ Web Server/src/app/system-access/system-access-services/auth.service.ts
@@ -12,7 +12,7 @@ export class AuthService {
   private _appServerConfig = require('../assets/configs/config-app-server.json')
 
   private _registerUrl = "http://"+this._appServerConfig.host+":"+this._appServerConfig.port+"/api/register"
-  private _loginUrl = "http://"+this._appServerConfig.host+":"+this._appServerConfig.port+"/routes/systems-access/login/login"
+  private _loginUrl = "http://"+this._appServerConfig.host+":"+this._appServerConfig.port+"/api/login/access"
   private _recoverPasswordUrl = "http://"+this._appServerConfig.host+":"+this._appServerConfig.port+"/api/recover-password"
   private _resetPasswordUrl = "http://"+this._appServerConfig.host+":"+this._appServerConfig.port+"/api/reset-password"
   private _sendEmailUrl = "http://"+this._appServerConfig.host+":"+this._appServerConfig.port+"/api/send-email"
